f1e4b4792d1f5d8bdcd6f8a1f4389bf87c0f825d
Chris Blossom <cbloss@gmail.com>
2020-03-26 14:58:58 -0700

    start of detect file

---
+++ src/utils/detect-file-type.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+
+const map = {
+    '.js': 'javascript',
+};
+
+function detectFileType(file: string) {
+    const parsed = path.parse(file);
+
+    console.log(parsed);
+}
+
+export { detectFileType };
